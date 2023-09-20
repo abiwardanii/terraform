2af07c35d46b99667350f8ae873baab2d1fda60a
abi wardani <abiwardani@abis-MacBook-Pro.local>
2023-09-21 01:15:27 +0700

    add create ami aws

--- main.tf
+++ main.tf
@@ -19,47 +19,11 @@ provider "aws" {
   region  = "ap-southeast-1"
 }
 
-//S3 Bucket
-resource "aws_s3_bucket" "compass" {
-  bucket = "compass-ecom-bucket"
-
+resource "aws_ami_from_instance" "compass" {
+  name               = "compass_ami"
+  source_instance_id = "i-0ed51a002edc6b739"
   tags = {
-    Name        = "Compass"
-    Project     = "Compass"
+    Name = "compass_fe"
+    Project = "Compass"
   }
-}
-
-//CRUD Policy
-resource "aws_s3_bucket_policy" "compass_policy" {
-  bucket = aws_s3_bucket.compass.id
-
-  policy = jsonencode({
-    Version = "2012-10-17",
-    Statement = [
-      {
-        Sid       = "CURD_Object",
-        Effect    = "Allow",
-        Principal = "*",
-        Action    = [
-          "s3:GetObject",
-          "s3:ListBucket",
-          "s3:PutObject",
-          "s3:DeleteObject",
-        ],
-        Resource  = [
-          aws_s3_bucket.compass.arn,
-          "${aws_s3_bucket.compass.arn}/*",
-        ],
-      },
-    ],
-  })
-}
-//Block access public
-resource "aws_s3_bucket_public_access_block" "compass" {
-  bucket = aws_s3_bucket.compass.id
-
-  block_public_acls       = true
-  block_public_policy     = true
-  ignore_public_acls      = true
-  restrict_public_buckets = true
 }
