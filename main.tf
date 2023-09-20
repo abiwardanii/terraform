ae12c942619189eb0450a23d19c2416f0d6a1f95
abi wardani <abiwardani@abis-MacBook-Pro.local>
2023-09-21 00:18:47 +0700

    update

--- main.tf
+++ main.tf
@@ -19,11 +19,47 @@ provider "aws" {
   region  = "ap-southeast-1"
 }
 
+//S3 Bucket
 resource "aws_s3_bucket" "compass" {
   bucket = "compass-ecom-bucket"
 
   tags = {
     Name        = "Compass"
-    Project = "Compass"
+    Project     = "Compass"
   }
+}
+
+//CRUD Policy
+resource "aws_s3_bucket_policy" "compass_policy" {
+  bucket = aws_s3_bucket.compass.id
+
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "CURD_Object",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = [
+          "s3:GetObject",
+          "s3:ListBucket",
+          "s3:PutObject",
+          "s3:DeleteObject",
+        ],
+        Resource  = [
+          aws_s3_bucket.compass.arn,
+          "${aws_s3_bucket.compass.arn}/*",
+        ],
+      },
+    ],
+  })
+}
+//Block access public
+resource "aws_s3_bucket_public_access_block" "compass" {
+  bucket = aws_s3_bucket.compass.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
